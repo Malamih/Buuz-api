0588833973e331162397d2beffee9c7fb55071d5
amer-wild11 <wild4bs@gmail.com>
2025-07-29 11:44:09 +0300

    edits

--- src/main.ts
+++ src/main.ts
@@ -47,6 +47,6 @@ async function bootstrap() {
     credentials: true,
     optionsSuccessStatus: 204,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000 | 3001 | 3002 | 3003);
 }
 bootstrap();
